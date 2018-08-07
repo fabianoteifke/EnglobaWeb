7399e63c2ca1a16c524fcb04761b8719d2dba406
Fabiano <teifkefabiano@gmail.com>
2018-08-07 19:19:26 -0300

    alteração da configuração do BD

--- config/conexao.php
+++ config/conexao.php
@@ -2,7 +2,7 @@
 
 //include_once "config.php";
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=engloba","root","");
+    $pdo = new PDO("mysql:host=localhost;dbname=aluno_engloba","root","");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Não foi possível se conectar ao banco!' . '<br/>';
--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 # Informações sobre o banco de dados:
 define("HOST_NAME" , "localhost");
-define("DATABASE_NAME" , "engloba");
+define("DATABASE_NAME" , "aluno_engloba");
 define("DATABASE_USER_NAME" , "root");
 define("DATABASE_PASSWORD", "");
